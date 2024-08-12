9894a5645b9bf748879a68a474ebe0d01a8d1328
Bhagyesh-20 <bhagyesh.dh20@gmail.com>
2024-08-12 20:58:51 +0530

    .

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-from fastapi import FastAPI,WebSocket
+from fastapi import FastAPI
+import uvicorn
+app = FastAPI()
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.get("/api")
+async def root():
+    return {"message": "API endpoint"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
